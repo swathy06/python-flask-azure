f1a1cd76d4bfeb92d209814dda9bcd0abfeefcb2
swathy06 <38415961+swathy06@users.noreply.github.com>
2018-07-14 11:27:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['POST'])
 def hello_world():
-  return 'Hey its Python Flask application!'
-
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+    req = request.get_json(silent=True, force=True)
+ 
+    print("Request:")
+    print(json.dumps(req, indent=4))
+ 
+    res = processRequest(req)
+ 
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+  #return 'Hey its Python Flask application!'
 
 
 if __name__ == '__main__':
