7a0fd1d0dec1e687d1418577649089033a48e3e7
swathy06 <38415961+swathy06@users.noreply.github.com>
2018-07-14 12:23:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   return 'Apple a day'
-@app.route('/a')
-def hello_world():
-  return 'Apple a day'
+
 
 if __name__ == '__main__':
   app.run()
